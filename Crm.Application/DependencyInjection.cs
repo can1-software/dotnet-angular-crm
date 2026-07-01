36e234d12247622e8a1effdd3588a67efab2efb8
Can Oğuzorhan <oguzorhancan1@gmail.com>
2026-07-01 21:03:10 +0300

    - Register application validators
    - Register FluentValidation with aligned package versions

--- Crm.Application/DependencyInjection.cs
+++ Crm.Application/DependencyInjection.cs
@@ -1,5 +1,6 @@
 using Crm.Application.Interfaces;
 using Crm.Application.Services;
+using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Crm.Application;
@@ -9,6 +10,7 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddScoped<ICompanyService, CompanyService>();
+        services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly);
         return services;
     }
 }
